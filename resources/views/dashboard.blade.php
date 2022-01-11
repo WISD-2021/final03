0025280ba160a926405a22d1b6bda592f9c1ad17
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2022-01-11 21:51:35 +0800

    登入後直接跳到student頁面

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
+            &emsp;<a href="/student" class="underline">Student</a>
         </h2>
     </x-slot>
 
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,24 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum','verified'])->get('/student',[App\Http\Controllers\StudentController::class,'index'])->name('student.index');
+
+Route::get('/teacher',function(){
+        return view('teacher');
+});
+
+Route::get('/course',function(){
+    return view('course');
+});
+
+//Route::prefix('admin')->group(function () {
+//    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.dashboard.index');  //進入後台管理介面的路由
+//
+//    Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');  //候台列出所有文章的路由
+//    Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');  //候台產生新增表單的路由
+//    Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');  //候台生產修改表單的路由
+//    Route::post('posts',[AdminPostsController::class,'store'])->name('admin.posts.store'); //新增資料
+//    Route::patch('posts/{post}',[AdminPostsController::class,'update'])->name('admin.posts.update'); //更新資料
+//    Route::delete('posts/{post}',[AdminPostsController::class,'destroy'])->name('admin.posts.destroy'); //刪除資料
+//});
