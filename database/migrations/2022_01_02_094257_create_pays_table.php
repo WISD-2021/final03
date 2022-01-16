1fd04c77bd375daee4e197beed919c76ccb34f3b
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2022-01-16 21:20:13 +0800

    修改pays資料表日期資料型態、名稱

--- database/migrations/2022_01_02_094257_create_pays_table.php
+++ database/migrations/2022_01_02_094257_create_pays_table.php
@@ -17,7 +17,7 @@ class CreatePaysTable extends Migration
             $table->id();
             $table->unsignedBigInteger('student_id');
             $table->unsignedBigInteger('homework_id');
-            $table->dateTime('datetime');
+            $table->date('date');
             $table->string('file',100);
             $table->integer('score')->nullable();
             $table->timestamps();
